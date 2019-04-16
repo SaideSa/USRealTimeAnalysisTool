10ec91a8a389154559574eb25e541c8484fb1f2b
Timo <timolahres@hotmail.com>
2019-04-16 14:30:34 +0200

    simple example GUI

--- src/gui/FirstClass.java
+++ src/gui/FirstClass.java
@@ -1,5 +1,23 @@
 package gui;
 
-public class FirstClass {
+import javax.swing.*;
+
+public class FirstClass extends JFrame{
+	
+	
+	public FirstClass() {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setLayout(null);
+		this.setTitle("USRealTimeAnalysisTool");
+		this.setResizable(false);
+		this.setSize(800,500);
+	    init();
+		
+	}
+	
+	public void init() {
+	}
+	
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
-public class Main {
+import gui.FirstClass;
 
+public class Main {
+	public static void main (String[] args) {
+	new FirstClass();
+	}
 }
