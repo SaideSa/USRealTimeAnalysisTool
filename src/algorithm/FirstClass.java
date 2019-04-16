2cbde0577ee31f439941ab7a17826b8286f57303
Timo <timolahres@hotmail.com>
2019-04-16 15:22:23 +0200

    berechnen klasse

--- src/algorithm/FirstClass.java
+++ src/algorithm/FirstClass.java
@@ -1,8 +1,13 @@
 package algorithm;
 
+import java.util.Vector;
+
 import javax.swing.*;
 
 public class FirstClass extends JFrame{
+	
+	private Vector dinge = new Vector();
+	
 	
 	public FirstClass() {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -16,6 +21,15 @@ public class FirstClass extends JFrame{
 	}
 	
 	public void init() {
+	}
+	
+	public void berechnen() {
+		Box tempbox1 = (Box) dinge.firstElement();
+		Box tempbox2 = (Box) dinge.lastElement();
+		Box tempbox3 = new Box(tempbox1.x, tempbox2.y);
+		double ankathete1 = tempbox3.y - tempbox1.y; 
+		double ankathete2 = tempbox3.x - tempbox2.x;
+		int entfernung = (int) Math.hypot(ankathete1,ankathete2);
 	}
 
 }
