f95e7ed8895fb9e8dab23b526a6237f46a70bc5e
HS-ULM\soeylemez <soeylemez@mail.hs-ulm.de>
2019-04-17 13:09:49 +0200

    Fenster öffnet sich mit Titel

--- src/gui/FirstClass.java
+++ src/gui/FirstClass.java
@@ -1,5 +1,16 @@
 package gui;
 
-public class FirstClass {
+import javafx.application.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class FirstClass extends Application {
+	   public void start(Stage s) throws Exception {
+	    	s.setTitle("USRealTimeAnalysisTool");		
+	    	Pane p = new Pane();					
+	    	s.setScene(new Scene(p, 500, 300));		
+	    	
+	    	s.show();
+	   }	
 }
