909b29ce3e9029025ed4b6a1ff8e01e39d2f87a4
Timo <timolahres@hotmail.com>
2019-04-17 12:25:48 +0200

    Calculation groß

--- src/algorithm/calculation.java
+++ src/algorithm/calculation.java
@@ -1,14 +1,5 @@
 package algorithm;
 
-public class calculation {
-
-	public calculation() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+public class Calculation {
 
 }
