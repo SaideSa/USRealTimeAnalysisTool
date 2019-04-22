904938ca16d7cbcd00507e4a698ffa07e9884bc9
Timo <timolahres@hotmail.com>
2019-04-22 13:43:06 +0200

    class berechnen und berechnen2 mouslistener implementierung entfernungsberechnungen

--- src/algorithm/Calculation.java
+++ src/algorithm/Calculation.java
@@ -1,5 +1,20 @@
 package algorithm;
 
 public class Calculation {
+	
+	public static int berechnen() {
+		Box tempbox1 = (Box) FirstClass.dinge.firstElement();
+		Box tempbox2 = (Box) FirstClass.dinge.lastElement();
+		Box tempbox3 = new Box(tempbox1.x, tempbox2.y);
+		int ankathete1 = tempbox3.y - tempbox1.y; 
+		int ankathete2 = tempbox3.x - tempbox2.x;
+		int entfernung = (int) Math.hypot(ankathete1,ankathete2);
+		return entfernung;
+	}
+	
+	
+	public static int berechnen2(int x1, int y1, int x2, int y2){
+	return (int) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2-y1), 2));
+	}
 
 }
--- src/algorithm/FirstClass.java
+++ src/algorithm/FirstClass.java
@@ -1,35 +1,89 @@
 package algorithm;
 
-import java.util.Vector;
+import java.awt.event.*;
+import java.util.*;
 
 import javax.swing.*;
 
-public class FirstClass extends JFrame{
+public class FirstClass extends JFrame implements MouseListener{
 	
-	private Vector dinge = new Vector();
+	static Vector dinge = new Vector();
+	
+
+	
+	
 	
 	
 	public FirstClass() {
+		
+		
+		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setLayout(null);
 		this.setTitle("USRealTimeAnalysisTool");
 		this.setResizable(false);
 		this.setSize(800,500);
+		this.addMouseListener(this);
 	    init();
 		
 	}
 	
 	public void init() {
+			
+		
 	}
 	
-	public void berechnen() {
-		Box tempbox1 = (Box) dinge.firstElement();
-		Box tempbox2 = (Box) dinge.lastElement();
-		Box tempbox3 = new Box(tempbox1.x, tempbox2.y);
-		double ankathete1 = tempbox3.y - tempbox1.y; 
-		double ankathete2 = tempbox3.x - tempbox2.x;
-		int entfernung = (int) Math.hypot(ankathete1,ankathete2);
-	}
+	
+	
+		
+			
+		
+	
+	
+	@Override
+	public void mousePressed(MouseEvent evt) {
 
+		int button = evt.getButton();
+		
+		if(dinge.size()<2) {
+			System.out.println(button);
+		}
+		
+			if (button == 1) {
+				if (dinge.size() < 2) {
+					dinge.addElement(new Box(evt.getX(), evt.getY()));
+				} else {
+				}
+			} else if (button == 3) {
+				if (dinge.size() > 0) {
+					dinge.remove(dinge.lastElement());
+				} else {
+				}
+			}
+		}
+	
+
+	@Override
+	public void mouseClicked(MouseEvent evt) {}
+
+	@Override
+	public void mouseEntered(MouseEvent evt) {}
+
+	@Override
+	public void mouseExited(MouseEvent evt) {}
+
+	@Override
+	public void mouseReleased(MouseEvent evt) {
+		if(evt.getButton()==1 && dinge.size()==2) {
+			if(dinge.size()==2) {
+				int ergebnis = Calculation.berechnen();
+				System.out.println("Die Entfernung beträgt " + ergebnis);
+			}
+		}
+	}
 }
+	
+	
+
+
