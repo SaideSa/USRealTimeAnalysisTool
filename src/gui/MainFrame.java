27b2ff09c5db48f658ece268f1288b4f3f463f3b
HS-ULM\soeylemez <soeylemez@mail.hs-ulm.de>
2019-04-23 14:46:50 +0200

    Kommentaren hinzugefügt und Panel umbenannt

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -11,35 +11,31 @@ import javafx.stage.Stage;
 public class MainFrame extends Application {
 	   public void start(Stage s) throws Exception {
 	    	s.setTitle("USRealTimeAnalysisTool");		
-	    	Pane p = new Pane();					
-	    	s.setScene(new Scene(p, 900, 500));		
+	    	Pane hauptfenster = new Pane();					
+	    	s.setScene(new Scene(hauptfenster, 900, 500));		// Hauptfenster ist 900cm (X-Koordinate) x 500cm (Y-Koordinate) groß 
 	    	
 	    	
-	    	Button berechnen = new Button("Berechnen");
-	    	p.getChildren().add(berechnen);
+	    	Button berechnen = new Button("Berechnen"); // Button zur Berechnung der X & Y Koordinaten des US-Bildes
+	    	hauptfenster.getChildren().add(berechnen);
 	    	berechnen.setText("Berechnen");
-	    	
-	    	berechnen.setLayoutX(500);
+	    	berechnen.setLayoutX(500); // Größe des Buttons 
 	    	berechnen.setLayoutY(50);
 	    	
-	    	Label ergebnis = new Label("Ergebnis:");
-	    	p.getChildren().add(ergebnis);
+	    	Label ergebnis = new Label("Ergebnis:"); // Erstellung des Labels "Ergebnis:" zur Ergebnisdarstellung der berechneten X und Y Koordinaten
+	    	hauptfenster.getChildren().add(ergebnis);
 	    	ergebnis.setLayoutX(500);		// X/Y Coodis angeben
 	    	ergebnis.setLayoutY(120);
 	    	
-	    	TextField tf = new TextField();
-	    	p.getChildren().add(tf);
-	    	tf.setLayoutX(600);
+	    	TextField tf = new TextField(); // Ausgabefeld des Ergebnisses
+	    	hauptfenster.getChildren().add(tf);
+	    	tf.setLayoutX(600); // Größe des Ausgabefeldes
 	    	tf.setLayoutY(120);
 	    	
-	    	Button reset = new Button("Reset");
-	    	p.getChildren().add(reset);
+	    	Button reset = new Button("Reset"); // Reset-Button zum Zurücksetzen der Ergebnisse
+	    	hauptfenster.getChildren().add(reset);
 	    	reset.setLayoutX(500);
 	    	reset.setLayoutY(80);
-	    	
-	    
-	    	
-	    	
+	    		    	
 	    	s.show();
 	   }	
 }
