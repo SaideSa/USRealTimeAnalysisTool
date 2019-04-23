fa5c856bd95c5b0ea340f00e62a68934c294cd3a
Timo <timolahres@hotmail.com>
2019-04-23 13:29:42 +0200

    Auslagerung des vektors auslagerung des Listener

--- src/algorithm/Calculation.java
+++ src/algorithm/Calculation.java
@@ -1,10 +1,15 @@
 package algorithm;
 
+import java.awt.event.*;
+import java.util.*;
+
 public class Calculation {
 	
+	static Vector dinge = new Vector();
+	
 	public static int berechnen() {
-		Box tempbox1 = (Box) FirstClass.dinge.firstElement();
-		Box tempbox2 = (Box) FirstClass.dinge.lastElement();
+		Box tempbox1 = (Box) dinge.firstElement();
+		Box tempbox2 = (Box) dinge.lastElement();
 		Box tempbox3 = new Box(tempbox1.x, tempbox2.y);
 		int ankathete1 = tempbox3.y - tempbox1.y; 
 		int ankathete2 = tempbox3.x - tempbox2.x;
@@ -17,4 +22,35 @@ public class Calculation {
 	return (int) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2-y1), 2));
 	}
 
+	public static void MouseListenerPressed(MouseEvent evt) {
+		int button = evt.getButton();
+		
+		if(dinge.size()<2) {
+			System.out.println(button);
+		}
+		
+			if (button == 1) {
+				if (dinge.size() < 2) {
+					dinge.addElement(new Box(evt.getX(), evt.getY()));
+				} else {
+				}
+			} else if (button == 3) {
+				if (dinge.size() > 0) {
+					dinge.remove(dinge.lastElement());
+				} else {
+				}
+			}
+		
+	}
+	
+	public static void MouseListenerReleased(MouseEvent evt) {
+		
+		if(evt.getButton()==1 && dinge.size()==2) {
+			if(dinge.size()==2) {
+				int ergebnis = Calculation.berechnen();
+				System.out.println("Die Entfernung beträgt " + ergebnis);
+			}
+		}
+	}
+	
 }
--- src/algorithm/FirstClass.java
+++ src/algorithm/FirstClass.java
@@ -1,22 +1,14 @@
 package algorithm;
 
 import java.awt.event.*;
-import java.util.*;
 
 import javax.swing.*;
 
 public class FirstClass extends JFrame implements MouseListener{
 	
-	static Vector dinge = new Vector();
-	
-
-	
-	
 	
 	
 	public FirstClass() {
-		
-		
 		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
@@ -30,39 +22,18 @@ public class FirstClass extends JFrame implements MouseListener{
 	}
 	
 	public void init() {
-			
-		
-	}
-	
-	
-	
-		
-			
-		
+		}
 	
 	
 	@Override
 	public void mousePressed(MouseEvent evt) {
-
-		int button = evt.getButton();
-		
-		if(dinge.size()<2) {
-			System.out.println(button);
-		}
-		
-			if (button == 1) {
-				if (dinge.size() < 2) {
-					dinge.addElement(new Box(evt.getX(), evt.getY()));
-				} else {
-				}
-			} else if (button == 3) {
-				if (dinge.size() > 0) {
-					dinge.remove(dinge.lastElement());
-				} else {
-				}
-			}
+		Calculation.MouseListenerPressed(evt);
 		}
 	
+	@Override
+	public void mouseReleased(MouseEvent evt) {
+		Calculation.MouseListenerReleased(evt);
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent evt) {}
@@ -73,15 +44,7 @@ public class FirstClass extends JFrame implements MouseListener{
 	@Override
 	public void mouseExited(MouseEvent evt) {}
 
-	@Override
-	public void mouseReleased(MouseEvent evt) {
-		if(evt.getButton()==1 && dinge.size()==2) {
-			if(dinge.size()==2) {
-				int ergebnis = Calculation.berechnen();
-				System.out.println("Die Entfernung beträgt " + ergebnis);
-			}
-		}
-	}
+	
 }
 	
 	
