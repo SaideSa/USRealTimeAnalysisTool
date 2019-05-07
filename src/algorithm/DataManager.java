a0b2b55ecf43e2b96827104144515988d0f1ea5e
Fwiest <Fwiest@mail.hs-ulm.de>
2019-05-07 14:31:04 +0200

    connect DataManger with HardwareGroup

--- src/algorithm/DataManager.java
+++ src/algorithm/DataManager.java
@@ -1,6 +1,7 @@
 package algorithm;
 
 import java.awt.Image;
+import InputOutput.VideoCapturing;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 //import javax.swing.ImageIcon; 
@@ -8,17 +9,17 @@ import org.opencv.core.Mat;
 
 public class DataManager {
 
-	public static Image toBufferedImage(Mat m) {
+	public static Image toBufferedImage(Mat frameMatrix) {
 	    int type = BufferedImage.TYPE_BYTE_GRAY;
 
-	    if (m.channels() > 1) {
+	    if (frameMatrix.channels() > 1) {
 	        type = BufferedImage.TYPE_3BYTE_BGR;
 	    }
 
-	    int bufferSize = m.channels() * m.cols() * m.rows();
+	    int bufferSize = frameMatrix.channels() * frameMatrix.cols() * frameMatrix.rows();
 	    byte[] b = new byte[bufferSize];
-	    m.get(0, 0, b); // get all the pixels
-	    BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
+	    frameMatrix.get(0, 0, b); // get all the pixels
+	    BufferedImage image = new BufferedImage(frameMatrix.cols(), frameMatrix.rows(), type);
 
 	    final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
 	    System.arraycopy(b, 0, targetPixels, 0, b.length);
@@ -26,7 +27,6 @@ public class DataManager {
 	    return image;
 		}
 	
-
 	}
 
 
