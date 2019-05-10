dec619b0366779ada589d69292d68ba9839ebc5c
TiziaF <tfiedler@mail.hs-ulm.de>
2019-05-10 14:56:26 +0200

    Revert "no message"
    This reverts commit 4675dceba3061940febee494d83cd4164ea9e89f.

--- src/algorithm/Measurement.java
+++ src/algorithm/Measurement.java
@@ -1,13 +1,5 @@
 package algorithm;
 
-public class Measurement extends Calculation {
+public class Measurement {
 
-	// DPI FEHLT! Aus Metadaten der Hardware? 
-	
-	public double conversion(){
-		// Pixelanzahl * 2,54[cm/i] / Auflösung [dpi] = Breite [cm] 
-		double converted = berechnen()*2.54;
-		return converted;
-	}
-	
 }
