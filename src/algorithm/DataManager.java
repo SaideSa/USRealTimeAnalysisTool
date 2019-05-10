719754ddc6106c80b51da24f5e2548ff7ecd0ec0
Fwiest <Fwiest@mail.hs-ulm.de>
2019-05-10 14:33:11 +0200

    BufferedImage

--- src/algorithm/DataManager.java
+++ src/algorithm/DataManager.java
@@ -4,34 +4,41 @@ import java.awt.Image;
 import inputOutput.VideoCapturing;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
-//import javax.swing.ImageIcon; 
+import javax.swing.ImageIcon; 
 import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
 
 public class DataManager {
 	// Klasse zur Formatierung von einer Matrix in ein Buffered Image
 
-	public static Image toBufferedImage(Mat frameMatrix) {
-	    int type = BufferedImage.TYPE_BYTE_GRAY;
-
-	    if (frameMatrix.channels() > 1) {
-	        type = BufferedImage.TYPE_3BYTE_BGR;
-	    }
-
-	    int bufferSize = frameMatrix.channels() * frameMatrix.cols() * frameMatrix.rows();
-	    byte[] b = new byte[bufferSize];
-	    frameMatrix.get(0, 0, b); // get all the pixels
-	    BufferedImage image = new BufferedImage(frameMatrix.cols(), frameMatrix.rows(), type);
-
-	    final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-	    System.arraycopy(b, 0, targetPixels, 0, b.length);
-
-	    return image;
-		}
+	public BufferedImage readBufImg() {
+		BufferedImage bufImg = (BufferedImage) HighGui.toBufferedImage(readMatFrame());
+		return bufImg;
 	
 	}
+	
+	
+}
 
 
 
+		/*public static Image toBufferedImage(Mat frameMatrix) {
+		    int type = BufferedImage.TYPE_BYTE_GRAY;
+
+		    if (frameMatrix.channels() > 1) {
+		        type = BufferedImage.TYPE_3BYTE_BGR;
+		    }
+
+		    int bufferSize = frameMatrix.channels() * frameMatrix.cols() * frameMatrix.rows();
+		    byte[] b = new byte[bufferSize];
+		    frameMatrix.get(0, 0, b); // get all the pixels
+		    BufferedImage image = new BufferedImage(frameMatrix.cols(), frameMatrix.rows(), type);
+
+		    final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		    System.arraycopy(b, 0, targetPixels, 0, b.length);
+
+		    return image;
+			}
 
 // BufferedImage skalieren
 /* BufferedImage ergebnis = new BufferedImage(skaliertebreite, skaliertehöhe, , BufferedImage.TYPE_INT_ARGB);
