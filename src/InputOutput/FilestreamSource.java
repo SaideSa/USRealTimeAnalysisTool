a45b0f8cb0fcffd439483f410d5b7ad8ec1043d4
pschugt <philippschugt@gmail.com>
2019-05-29 11:09:27 +0200

    Klasse und ihre Methoden durch kommentiert

--- src/InputOutput/FilestreamSource.java
+++ src/InputOutput/FilestreamSource.java
@@ -8,21 +8,37 @@ import org.opencv.highgui.HighGui;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.Videoio;
 
-
-
+ /**
+    * provides the  footage from a file with these formats:
+    * mp4, avi, mkv, mov, 3GP, jpg and png
+    * 
+    * @author Team 3
+    *
+    */
 public class FilestreamSource extends AbstractImageSource{
 
   private VideoCapture vc;
   public BufferedImage bufImg = null;
   public int fps;
   private String path;
-
+ 
+  /**
+	 * constructs a new FilestreamSource object with the transmitted <code>path</code>.
+	 * 
+	 * @param path describes the path of the file 
+	 */
   public FilestreamSource(String path) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		frameMatrix = new Mat();
 		this.path = path;
 	}
 
+  /**
+	 * initializes a new VideoCapture object using the <code>path</code>. 
+	 * The video or image is opened.
+	 * 
+	 * @return whether the connection was successful or not
+	 */
 	public boolean openConnection() {
 		System.out.println("capFile");
 		vc = new VideoCapture(path);
@@ -38,6 +54,11 @@ public class FilestreamSource extends AbstractImageSource{
 		return isConnected;
 	}
 	
+	/**
+	 * Transmits the matrix of the picture
+	 * 
+	 * @return <code>frameMatrix</code>
+	 */	
 	public Mat getNextMat() { 
 		System.out.println((int) vc.get(Videoio.CAP_PROP_FPS));
 		fps = (int) vc.get(Videoio.CAP_PROP_FPS);
@@ -52,13 +73,18 @@ public class FilestreamSource extends AbstractImageSource{
 	}
 
 	
-	public BufferedImage readBufImg() {
+//	public BufferedImage readBufImg() {
+//	
+//		bufImg = (BufferedImage) HighGui.toBufferedImage(getNextMat());
+//		return bufImg;
+//
+//	}
 	
-		bufImg = (BufferedImage) HighGui.toBufferedImage(getNextMat());
-		return bufImg;
-
-	}
-	
+	/**
+	 *sets <code>isConnected = false</code>
+	 * 
+	 * @return <code>isConnected = false</code>
+	 */
 	public boolean closeConnection() {
 		isConnected=false;
 		return isConnected;
