dc4e12c0ac0437ec2b45cbaa77ca9d8bc251565e
TiziaF <tfiedler@mail.hs-ulm.de>
2019-06-25 15:11:02 +0200

    Umrechnung Pixel in mm in Calculaton / Measurement Umrechnung Ansatz gelöscht

--- src/algorithm/Measurement.java
+++ src/algorithm/Measurement.java
@@ -2,10 +2,6 @@ package algorithm;
 
 public class Measurement extends Calculation {
 
-	public double conversion(){
-		// Pixelanzahl * 2,54[cm/i] / Auflösung [dpi] = Breite [cm] 
-		double converted = berechnen()*2.54;
-		return converted;
-	}
+
 	
 }
