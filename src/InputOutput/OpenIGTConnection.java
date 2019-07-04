c531141ab3d5fbfe6d3a98b3af965c1253d1f8f0
Saide <saideshn63@gmail.com>
2019-07-04 21:48:28 +0200

    Java doc zu OpenIGTImage und -Connection ergänzt und Framerate für die OpenIGTLink Verbindung jetzt auche ersichtlich.

--- src/InputOutput/OpenIGTConnection.java
+++ src/InputOutput/OpenIGTConnection.java
@@ -23,7 +23,10 @@ public class OpenIGTConnection implements IOpenIgtPacketListener {
 	private ImageMessage imgMsg;
 	private String name;
 	private byte[] imgData;
-
+	private long time1;
+	private long time2;
+	private long timeRes;
+	public long fps;
 	
 	
 	/**
@@ -41,14 +44,21 @@ public class OpenIGTConnection implements IOpenIgtPacketListener {
 			
 			e.printStackTrace();
 		}
+		time1 = System.currentTimeMillis();
 
 
 	}
 
+	/**
+	 * This method is receiving an image as ImageMessage from the server, while it is running in the background during the connection.
+	 * After getting an ImageMessage, the <code>setImageMessage(ImageMessage img)</code> method is called in order to set the received ImageMessage and the body of ImageMessage is unpacked in order to get the image data, which is also set by calling <code>setImageData(byte[] imgData)</code>.
+	 * The framerate is also determined here.
+	 */
 	@Override
 	public void onRxImage(String name, ImageMessage image) {
+
 		setImageMessage(image);
-		
+
 		try {
 			
 			image.UnpackBody();
@@ -61,48 +71,62 @@ public class OpenIGTConnection implements IOpenIgtPacketListener {
 			e.printStackTrace();
 		}
 		this.name = name;
+		
+		time2 = System.currentTimeMillis();
+		timeRes = time2-time1;
+		time1= System.currentTimeMillis();
+		fps = 1000/timeRes;
 
 	}
-
+	
+	/**
+	 * This method is for getting the current ImageMessage.
+	 * @return ImageMessage
+	 */
 	public ImageMessage getImageMessage() {
 		return imgMsg;
 	}
 
+	/**
+	 * This method is for setting an ImageMessage.
+	 */
 	public void setImageMessage(ImageMessage img) {
 		imgMsg = img;
 	}
 	
+	/**
+	 * This method is for setting an image data in the form of a byte.
+	 */
 	public void setImageDataByte(byte[] imgData) {
 		this.imgData = imgData;
 	}
 	
-	
+	/**
+	 * This method is for getting the current image data.
+	 * @return It returns the image data as a byte[]
+	 */
 	public byte[] getImageDataByte() {
 
 		return imgData;
 	}
 
+	/**
+	 * This method is for stopping the connection (the client).
+	 * @return <code>client.isInterrupted()</code> is true if closing the connection was successful
+	 */
 	public boolean stop() {
 		client.stopClient();
 		return client.isInterrupted();
 	}
 	
+	/**
+	 * This method is for checking the connection while the application is running.
+	 * @return <code>isConnected</code>
+	 */
 	public boolean isConnected() {
 		return client.isConnected();
 	}
-	
-//	public long getWidth() {
-//		return dimensions[0];	
-//	}
-//
-//	public long getHeight() {
-//		return dimensions[1];	
-//	}
-//	
-//	public long getType() {
-//		return dimensions[2];	
-//	}
-//	
+
 	/**
 	 * The methods below are not used during a connection over MITK
 	 */
--- src/InputOutput/OpenIGTImageSource.java
+++ src/InputOutput/OpenIGTImageSource.java
@@ -9,7 +9,8 @@ import org.opencv.core.Mat;
 
 
 /**
- * 
+ * This class is inherited from AbstractImageSource and provides the basic functionalities for the application like opening and closing the connection, and an image converted to a Mat object from OpenCV. 
+ * The class OpenIGTConnection is needed here for establishing the connection. 
  * @author team3
  *
  */
@@ -20,17 +21,23 @@ public class OpenIGTImageSource extends AbstractImageSource {
 	private ImageMessage imgMsg;
 	private byte[] imgData;
 	private long[] dimensions;
-	private int rows = 0;
-	private int cols = 0 ;
-	private int type = 1;
+	private int rows;
+	private int cols;
+	private int type;
 	
 	
-	
+	/**
+	 * This constructor is used when the user doesn't enter an ip address or a port number. The default value are used then.
+	 */
 	public OpenIGTImageSource() {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
 	}
 	
+	/**
+	 * This constructor allows the user to enter an <code>ipAdress</code> and <code>port</code> number for the connection.
+	 * @param ipAddress
+	 * @param port
+	 */
 	public OpenIGTImageSource(String ipAddress, int port) {
 		ip = ipAddress;
 		this.port = port;
@@ -39,6 +46,12 @@ public class OpenIGTImageSource extends AbstractImageSource {
 		
 	}
 
+	/**
+	 * This method is starting the connection by creating an object of OpenIGTConnection. A </code>ip</code> address and <code>port</code> number is necessary. <code>Thread.sleep</code> is needed here to wait (2 seconds) for the connection to establish. 
+	 * The height and width of the image adapts here to the given dimensions from MITK by usig the <code>getDimensions</code> method from the OpenIGT library. The parameter 0 returns the width, 1 returns the height and 2 returns the type.   
+	 *  
+	 * @return whether the connection was successful or not
+	 */
 	public boolean openConnection() {
 		
 		igtConnection = new OpenIGTConnection(ip, port);
@@ -60,7 +73,11 @@ public class OpenIGTImageSource extends AbstractImageSource {
 	}
 
 	
-	
+	/**
+	 * This method is for closing the connection. It calls the <code>stop()</code method from OpenIGTConnection
+	 * 
+	 * @return <code>exit</code> is true if closing the connection was successful
+	 */
 	public boolean closeConnection() {
 		igtConnection.stop();
 		isConnected = false;
@@ -68,38 +85,77 @@ public class OpenIGTImageSource extends AbstractImageSource {
 		return exit;
 	}
 
+	/**
+	 * This method is getting the current ImageMessage if it's needed.
+	 * 
+	 * @return <code>imgMsg</code>
+	 */
 	public ImageMessage getImageMessage() {
 		imgMsg = igtConnection.getImageMessage();
 		return imgMsg;
 	}
 
+	/**
+	 * This method is getting the <code>imgData</code> as byte array (with a length of rows*cols), and then it is converted to a Mat object from OpenCV by using <code>put(int row, int col, byte[])</code>.
+	 * 
+	 * @return Mat (from the OpenCV library)
+	 */
 	public Mat getNextMat() {
-		
-//		System.out.println(rows + " " + cols);
 		imgData = igtConnection.getImageDataByte();
 		
 		frameMatrix.put(0, 0, imgData);
-	
+
+		fps = (int) igtConnection.fps;
 		return frameMatrix;
 	}
 	
 	
 
+	/**
+	 * This method is for checking the connection while the application is running by calling the <code>isConnected()</code> method from OpenIGTConnection.
+	 * @return <code>isConnected</code>
+	 */
 	public boolean checkConnection() {
 		isConnected = igtConnection.isConnected();
 		
 		return isConnected;
 	}
 	
+	/**
+	 * This method returns information about the type of ImageMessage.
+	 * 
+	 * @return the scalar type as a long
+	 */
 	public long getImageScalarType() {
 		return imgMsg.getScalarType();
 	}
 	
+	
+	/**
+	 * This method returns information about spacing of ImageMessage.
+	 * 
+	 * @return spacing as double[]
+	 */
 	public double[] getSpacing() {
 		return imgMsg.getSpacing();
 	}
 	
+	/**
+	 * This method returns information about the origin of ImageMessage.
+	 * 
+	 * @return origin as double[]
+	 */
 	public double[] getOrigin() {
 		return imgMsg.getOrigin();
 	}
+	
+	/**
+	 * This method returns the frames per second.
+	 * 
+	 * @return <code>fps</code> as a int
+	 */
+	public int getFPS() {
+		return fps;
+	}
 }
+
